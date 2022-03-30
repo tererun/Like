f8b5fcfc9be7eaae6695243f01f9b1e320c3844e
tererun <27857171+tererun@users.noreply.github.com>
2022-03-30 16:15:15 +0900

    :recycle: HDAPIを呼び出せるように

--- src/main/java/amata1219/like/Main.java
+++ src/main/java/amata1219/like/Main.java
@@ -17,6 +17,7 @@ import amata1219.like.playerdata.PlayerDatabase;
 import amata1219.like.task.TourRegularNotificationTask;
 import amata1219.like.tuplet.Tuple;
 import at.pcgamingfreaks.UUIDConverter;
+import me.filoghost.holographicdisplays.api.beta.HolographicDisplaysAPI;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -37,163 +38,174 @@ import java.util.List;
 import java.util.UUID;
 
 public class Main extends JavaPlugin {
-	
-	private static Main plugin;
-	
-	public static final HashMap<UUID, String> UUID_CACHE = new HashMap<>();
-	
-	public static final String INVITATION_TOKEN = UUID.randomUUID().toString();
-	public static final String OPERATOR_PERMISSION = "like.likeop";
-	
-	public static Main plugin(){
-		return plugin;
-	}
-	
-	public static String nameFrom(UUID uuid){
-		String name = UUID_CACHE.get(uuid);
-		if(name != null) return name;
-		name = UUIDConverter.getNameFromUUID(uuid);
-		UUID_CACHE.put(uuid, name);
-		return name;
-	}
-	
-	private Economy economy;
-	
-	private final HashMap<String, CommandExecutor> executors = new HashMap<>();
-	
-	private MainConfig config;
-	private LikeDatabase likeDatabase;
-	private PlayerDatabase playerDatabase;
-	private LikeLimitDatabase likeLimitDatabase;
-	private BookmarkDatabase bookmarkDatabase;
-	private TourConfig tourConfig;
-	
-	public final HashMap<Long, Like> likes = new HashMap<>();
-	public final LikeMap likeMap = new LikeMap();
-	public final HashMap<UUID, PlayerData> players = new HashMap<>();
-	public final HashMap<String, Bookmark> bookmarks = new HashMap<>();
-	public final HashMap<UUID, Long> descriptionEditors = new HashMap<>();
-	public final HashSet<UUID> cooldownMap = new HashSet<>();
 
-	public ControlLikeViewListener controlLikeViewListener;
+    private static Main plugin;
+    private static HolographicDisplaysAPI holographicDisplaysAPI;
 
-	public BukkitTask tourRegularNotificationTask;
+    public static final HashMap<UUID, String> UUID_CACHE = new HashMap<>();
 
-	@Override
-	public void onEnable(){
-		plugin = this;
-		
-		Plugin vault = getServer().getPluginManager().getPlugin("Vault");
-		if (vault == null) throw new NullPointerException("Not found Vault.");
+    public static final String INVITATION_TOKEN = UUID.randomUUID().toString();
+    public static final String OPERATOR_PERMISSION = "like.likeop";
 
-		RegisteredServiceProvider<Economy> provider = getServer().getServicesManager().getRegistration(Economy.class);
-		if(provider == null) throw new NullPointerException("Not found Vault.");
+    public static Main plugin() {
+        return plugin;
+    }
 
-		economy = provider.getProvider();
+    public static HolographicDisplaysAPI getHolographicDisplaysAPI() {
+        return holographicDisplaysAPI;
+    }
 
-		Field acceptingNew;
-		try {
-			acceptingNew = Enchantment.class.getDeclaredField("acceptingNew");
-			acceptingNew.setAccessible(true);
-			acceptingNew.set(null, true);
-			Enchantment.registerEnchantment(GleamEnchantment.INSTANCE);
-			acceptingNew.set(null, false);
-			acceptingNew.setAccessible(false);
-		} catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException ignored) {
+    public static String nameFrom(UUID uuid) {
+        String name = UUID_CACHE.get(uuid);
+        if (name != null) return name;
+        name = UUIDConverter.getNameFromUUID(uuid);
+        UUID_CACHE.put(uuid, name);
+        return name;
+    }
 
-		}
+    private Economy economy;
 
-		registerEventListeners(
-			new UIListener(),
-			new CreatePlayerDataListener(),
-			new EditingLikeDescriptionListener(),
-			controlLikeViewListener = new ControlLikeViewListener()
-		);
-		
-		config = new MainConfig();
-		
-		executors.put("like", new LikeCommand());
-		executors.put("likec", LikeCreationCommand.INSTANCE);
-		executors.put("likel", LikeListCommand.INSTANCE);
-		executors.put("likes", new LikeStatusCommand());
-		executors.put("liketoken", new LikeTokenCommand());
-		executors.put("likeb", new BookmarkCommand());
-		executors.put("likeop", new LikeOperatorCommand());
-		executors.put("likerandom", new LikeRandomCommand());
-		executors.put("liket", LikeTourCommand.INSTANCE);
-		executors.put("likesearch", new LikeSearchCommand());
-		executors.put("likeorscu", new OpenRangeSearchConfirmationUICommand());
-		
-		getServer().getScheduler().runTaskLater(this, () -> {
-			likeDatabase = new LikeDatabase();
-			Tuple<HashMap<Long, Like>, HashMap<UUID, List<Like>>> maps = likeDatabase.readAll();
-			maps.first.forEach(likes::put);
+    private final HashMap<String, CommandExecutor> executors = new HashMap<>();
 
-			for (Like like : likes.values()) likeMap.put(like);
-			
-			playerDatabase = new PlayerDatabase();
-			playerDatabase.readAll(maps.second).forEach(players::put);
-			
-			likeLimitDatabase = new LikeLimitDatabase();
-			bookmarkDatabase = new BookmarkDatabase();
-			bookmarkDatabase.readAll().forEach(bookmarks::put);
+    private MainConfig config;
+    private LikeDatabase likeDatabase;
+    private PlayerDatabase playerDatabase;
+    private LikeLimitDatabase likeLimitDatabase;
+    private BookmarkDatabase bookmarkDatabase;
+    private TourConfig tourConfig;
 
-			tourConfig = new TourConfig();
-			if (tourConfig.notificationIsEnabled()) tourRegularNotificationTask = getServer().getScheduler().runTaskTimerAsynchronously(this, new TourRegularNotificationTask(tourConfig), tourConfig.notificationIntervalTicks(), tourConfig.notificationIntervalTicks());
-		}, 15);
-	}
-	
-	@Override
-	public void onDisable(){
-		if (tourRegularNotificationTask != null) tourRegularNotificationTask.cancel();
+    public final HashMap<Long, Like> likes = new HashMap<>();
+    public final LikeMap likeMap = new LikeMap();
+    public final HashMap<UUID, PlayerData> players = new HashMap<>();
+    public final HashMap<String, Bookmark> bookmarks = new HashMap<>();
+    public final HashMap<UUID, Long> descriptionEditors = new HashMap<>();
+    public final HashSet<UUID> cooldownMap = new HashSet<>();
 
-		bookmarkDatabase.writeAll();
-		likeLimitDatabase.update();
-		playerDatabase.writeAll();
-		likeDatabase.writeAll();
+    public ControlLikeViewListener controlLikeViewListener;
 
-		for (Player player : getServer().getOnlinePlayers()) {
-			if (player.getOpenInventory().getTopInventory().getHolder() instanceof Layout) player.closeInventory();
-		}
+    public BukkitTask tourRegularNotificationTask;
 
-		HandlerList.unregisterAll(this);
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-		return executors.get(command.getName()).onCommand(sender, command, label, args);
-	}
-	
-	private void registerEventListeners(Listener... listeners){
-		for(Listener listener : listeners) getServer().getPluginManager().registerEvents(listener, this);
-	}
-	
-	public Economy economy(){
-		return economy;
-	}
-	
-	public MainConfig config(){
-		return config;
-	}
-	
-	public LikeDatabase likeDatabase(){
-		return likeDatabase;
-	}
-	
-	public PlayerDatabase playerDatabase(){
-		return playerDatabase;
-	}
-	
-	public LikeLimitDatabase likeLimitDatabase(){
-		return likeLimitDatabase;
-	}
-	
-	public BookmarkDatabase bookmarkDatabase(){
-		return bookmarkDatabase;
-	}
+    @Override
+    public void onEnable() {
+        plugin = this;
 
-	public TourConfig tourConfig() {
-		return tourConfig;
-	}
+        Plugin vault = getServer().getPluginManager().getPlugin("Vault");
+        if (vault == null) throw new NullPointerException("Not found Vault.");
+
+        RegisteredServiceProvider<Economy> provider = getServer().getServicesManager().getRegistration(Economy.class);
+        if (provider == null) throw new NullPointerException("Not found Vault.");
+
+        economy = provider.getProvider();
+
+        if (!getServer().getPluginManager().isPluginEnabled("HolographicDisplays"))
+            throw new NullPointerException("Not found HolographicDisplays.");
+
+        holographicDisplaysAPI = HolographicDisplaysAPI.get(Main.plugin());
+
+        Field acceptingNew;
+        try {
+            acceptingNew = Enchantment.class.getDeclaredField("acceptingNew");
+            acceptingNew.setAccessible(true);
+            acceptingNew.set(null, true);
+            Enchantment.registerEnchantment(GleamEnchantment.INSTANCE);
+            acceptingNew.set(null, false);
+            acceptingNew.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException | IllegalArgumentException ignored) {
+
+        }
+
+        registerEventListeners(
+                new UIListener(),
+                new CreatePlayerDataListener(),
+                new EditingLikeDescriptionListener(),
+                controlLikeViewListener = new ControlLikeViewListener()
+        );
+
+        config = new MainConfig();
+
+        executors.put("like", new LikeCommand());
+        executors.put("likec", LikeCreationCommand.INSTANCE);
+        executors.put("likel", LikeListCommand.INSTANCE);
+        executors.put("likes", new LikeStatusCommand());
+        executors.put("liketoken", new LikeTokenCommand());
+        executors.put("likeb", new BookmarkCommand());
+        executors.put("likeop", new LikeOperatorCommand());
+        executors.put("likerandom", new LikeRandomCommand());
+        executors.put("liket", LikeTourCommand.INSTANCE);
+        executors.put("likesearch", new LikeSearchCommand());
+        executors.put("likeorscu", new OpenRangeSearchConfirmationUICommand());
+
+        getServer().getScheduler().runTaskLater(this, () -> {
+            likeDatabase = new LikeDatabase();
+            Tuple<HashMap<Long, Like>, HashMap<UUID, List<Like>>> maps = likeDatabase.readAll();
+            maps.first.forEach(likes::put);
+
+            for (Like like : likes.values()) likeMap.put(like);
+
+            playerDatabase = new PlayerDatabase();
+            playerDatabase.readAll(maps.second).forEach(players::put);
+
+            likeLimitDatabase = new LikeLimitDatabase();
+            bookmarkDatabase = new BookmarkDatabase();
+            bookmarkDatabase.readAll().forEach(bookmarks::put);
+
+            tourConfig = new TourConfig();
+            if (tourConfig.notificationIsEnabled())
+                tourRegularNotificationTask = getServer().getScheduler().runTaskTimerAsynchronously(this, new TourRegularNotificationTask(tourConfig), tourConfig.notificationIntervalTicks(), tourConfig.notificationIntervalTicks());
+        }, 15);
+    }
+
+    @Override
+    public void onDisable() {
+        if (tourRegularNotificationTask != null) tourRegularNotificationTask.cancel();
+
+        bookmarkDatabase.writeAll();
+        likeLimitDatabase.update();
+        playerDatabase.writeAll();
+        likeDatabase.writeAll();
+
+        for (Player player : getServer().getOnlinePlayers()) {
+            if (player.getOpenInventory().getTopInventory().getHolder() instanceof Layout) player.closeInventory();
+        }
+
+        HandlerList.unregisterAll(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return executors.get(command.getName()).onCommand(sender, command, label, args);
+    }
+
+    private void registerEventListeners(Listener... listeners) {
+        for (Listener listener : listeners) getServer().getPluginManager().registerEvents(listener, this);
+    }
+
+    public Economy economy() {
+        return economy;
+    }
+
+    public MainConfig config() {
+        return config;
+    }
+
+    public LikeDatabase likeDatabase() {
+        return likeDatabase;
+    }
+
+    public PlayerDatabase playerDatabase() {
+        return playerDatabase;
+    }
+
+    public LikeLimitDatabase likeLimitDatabase() {
+        return likeLimitDatabase;
+    }
+
+    public BookmarkDatabase bookmarkDatabase() {
+        return bookmarkDatabase;
+    }
+
+    public TourConfig tourConfig() {
+        return tourConfig;
+    }
 
 }
